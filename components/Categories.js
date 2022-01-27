57b2b1737c3b1e8222e712c8d771e7247fb38374
Aditya Goyal <adityagoyaldoon@gmail.com>
2022-01-27 16:40:31 +0530

    Categories component complete

--- components/Categories.js
+++ components/Categories.js
@@ -14,6 +14,10 @@ const items = [{
     text: "Bakery Items",
 },
 {
+    image: require("../assets/images/desserts.png"),
+    text: "Desserts",
+},
+{
     image: require("../assets/images/fast-food.png"),
     text: "Fast Foods",
 },
@@ -21,15 +25,26 @@ const items = [{
 
 export default function Categories() {
   return (
-    <ScrollView horizontal>
-        <View style={{alignItems: "center", marginRight: 30,}}>
-            <Image source={items[0].image} style={{width: 50, height: 40, resize: "contain"}}/>
-            <Text style={{
-                fontSize: 13,
-                fontWeight: "900",
-            }}>{items[0].text}
-            </Text>
-      </View>
-    </ScrollView>
+      <View style={{
+          marginTop: 5,
+          backgroundColor: "white",
+          paddingVertical: 10, 
+          paddingLeft: 20,
+      }}>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+
+            {/* loop starts here */}
+            {items.map((item, index) => (
+                <View key={index} style={{alignItems: "center", marginRight: 30,}}>
+                    <Image source={item.image} style={{width: 50, height: 40, resize: "contain"}}/>
+                    <Text style={{
+                        fontSize: 13,
+                        fontWeight: "900",
+                    }}>{item.text}
+                    </Text>
+                </View>
+        ))}
+        </ScrollView>
+    </View>
   );
 }
