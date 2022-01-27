2575f0707a77f8061b113c6dd74969d788b01cc0
Aditya Goyal <adityagoyaldoon@gmail.com>
2022-01-27 16:24:22 +0530

    Categories created

--- components/Categories.js
+++ components/Categories.js
@@ -1,10 +1,35 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image, ScrollView } from 'react-native';
 import React from 'react';
+
+const items = [{
+    image: require("../assets/images/shopping-bag.png"),
+    text: "Pick-up",
+},
+{
+    image: require("../assets/images/soft-drink.png"),
+    text: "Soft Drinks",
+},
+{
+    image: require("../assets/images/bread.png"),
+    text: "Bakery Items",
+},
+{
+    image: require("../assets/images/fast-food.png"),
+    text: "Fast Foods",
+},
+];  
 
 export default function Categories() {
   return (
-    <View>
-      
-    </View>
+    <ScrollView horizontal>
+        <View style={{alignItems: "center", marginRight: 30,}}>
+            <Image source={items[0].image} style={{width: 50, height: 40, resize: "contain"}}/>
+            <Text style={{
+                fontSize: 13,
+                fontWeight: "900",
+            }}>{items[0].text}
+            </Text>
+      </View>
+    </ScrollView>
   );
 }
